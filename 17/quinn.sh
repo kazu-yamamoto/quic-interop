02af82daa4bbe76f3ac5eab333980e28108ff672
Kazu Yamamoto <kazu@iij.ad.jp>
2020-03-09 12:05:40 +0900

    adding quinn.sh.

---
+++ 17/quinn.sh
@@ -0,0 +1,6 @@
+# VHDRZQ
+./client -d ralith.com 4433 index.html -V
+./client -d ralith.com 4433 index.html
+./Client -d ralith.com 4433 index.html -R
+./client -d ralith.com 4433 index.html -Z
+./client -d ralith.com 4433 index.html -Q
