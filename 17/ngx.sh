fe8f652a03b48b9dafc69c815f67ddace48736f3
Kazu Yamamoto <kazu@iij.ad.jp>
2020-03-09 11:59:30 +0900

    adding ngx.sh.

---
+++ 17/ngx.sh
@@ -0,0 +1,6 @@
+# VHDRZQ
+./client -d cloudflare-quic.com 443 -V
+./client -d cloudflare-quic.com 443
+./client -d cloudflare-quic.com 443 -R
+./client -d cloudflare-quic.com 443 -Z
+./client -d cloudflare-quic.com 443 -Q
