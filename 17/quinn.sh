f6cdafa764a790d61605c070911a9f089e37d632
Kazu Yamamoto <kazu@iij.ad.jp>
2020-03-09 12:16:47 +0900

    update.

--- 17/quinn.sh
+++ 17/quinn.sh
@@ -1,6 +1,7 @@
-# VHDRZQ
+# VHDRZSQ
 ./client -d ralith.com 4433 index.html -V
 ./client -d ralith.com 4433 index.html
 ./Client -d ralith.com 4433 index.html -R
 ./client -d ralith.com 4433 index.html -Z
+./client -d ralith.com 4434 index.html
 ./client -d ralith.com 4433 index.html -Q
