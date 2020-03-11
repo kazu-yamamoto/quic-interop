8c289d6e1949692d8f7b4001cc23bcf185742b85
Kazu Yamamoto <kazu@iij.ad.jp>
2020-03-11 11:17:04 +0900

    update.

---
+++ 17/gquic.sh
@@ -0,0 +1,11 @@
+# VHDRQ B
+# cannot do 0RTT
+./client -d quic.rocks 4433 -V
+./client -d quic.rocks 4433
+./client -d quic.rocks 4433 -R
+./client -d quic.rocks 4433 -Z
+./client -d quic.rocks 4433 -Q
+./client -d quic.rocks 4433 -M
+./client -d quic.rocks 4433 -B
+./client -d quic.rocks 4433 -A
+
