3449fc8a210d379619a26d0030d980a62b1f33ef
Kazu Yamamoto <kazu@iij.ad.jp>
2021-12-09 10:34:50 +0900

    Client: allow version change only once

--- 20/local.sh
+++ 20/local.sh
@@ -1,10 +1,10 @@
 # VHDRZS
-./client -d 127.0.0.1 4433 -V
-./client -d 127.0.0.1 4433
-./client -d 127.0.0.1 4433 -R
-./client -d 127.0.0.1 4433 -Z
-./client -d 127.0.0.1 4433 -Q
-./client -d 127.0.0.1 4433 -M
-./client -d 127.0.0.1 4433 -N
-./client -d 127.0.0.1 4433 -B
-./client -d 127.0.0.1 4433 -A
+./client -d 127.0.0.1 443 -V
+./client -d 127.0.0.1 443
+./client -d 127.0.0.1 443 -R
+./client -d 127.0.0.1 443 -Z
+./client -d 127.0.0.1 443 -Q
+./client -d 127.0.0.1 443 -M
+./client -d 127.0.0.1 443 -N
+./client -d 127.0.0.1 443 -B
+./client -d 127.0.0.1 443 -A
--- 20/msquic.sh
+++ 20/msquic.sh
@@ -1,6 +1,6 @@
 # VHDRZSQ MNBA
 ./client -d quic.westus.cloudapp.azure.com 4433 -V
-./client -d quic.westus.cloudapp.azure.com 443
+./client -d quic.westus.cloudapp.azure.com 4433
 ./client -d quic.westus.cloudapp.azure.com 4433 -R
 ./client -d quic.westus.cloudapp.azure.com 4433 -Z
 ./client -d quic.westus.cloudapp.azure.com 4434 -S
